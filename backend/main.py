59f82e0a5012e8cbb35c8ec096664196b63afacb
Prince-Leo-home <a061804050713@163.com>
2025-10-19 12:41:37 +0800

    vim提交

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 # 允许跨域访问（前端可以直接请求）
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["mentalage.jakestar.cloud"],  # 生产环境建议改为具体域名
+    allow_origins=["*"],  # 生产环境建议改为具体域名
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/")
+def read_root():
+    return {"messages": "fuck"}
